064bdaaff8af93d30084000998e055db46f23558
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2020-02-03 17:35:34 -0500

    added layerControl to toggle between layers

--- js/scripts.js
+++ js/scripts.js
@@ -39,13 +39,13 @@ map.on('load', function() {
     'layout': {
       'line-join': 'round',
       'line-cap': 'round',
+      'visibility': 'visible',
       },
     'paint': {
         'line-color': {
           'property': 'BusRidership18.csvt_Yr2018',
           'stops': [
-            [0,  '#eff3ff'],
-            [3000000, '#bdd7e7'],
+            [3000000, '#ffffff'],
             [6000000, '#6baed6'],
             [9000000, '#3182bd'],
             [10000000,'#08519c'],
@@ -65,6 +65,7 @@ map.on('load', function() {
     'layout': {
       'line-join': 'round',
       'line-cap': 'round',
+      // 'visibility': 'none',
       },
     'paint': {
       'line-color': '#339966',
@@ -79,9 +80,44 @@ map.on('load', function() {
       'type': 'geojson',
       'data': citibikeUrl,
       },
+    'layout': {
+      // 'visibility': 'none',
+    },
     'paint': {
       'circle-radius': 3.2,
       'circle-color': '#666699'
     }
-});
+})
+
+  var toggleableLayerIds = ['Buses', 'Bikes', 'CitiBike'];
+
+  for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+    link.onclick = function(e) {
+      var clickedLayer = this.textContent;
+      e.preventDefault();
+      e.stopPropagation();
+
+      var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+      if (visibility === 'visible') {
+      map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+      this.className = '';
+      } else {
+      this.className = 'active';
+      map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+      }
+
+    };
+
+    var layers = document.getElementById('layerControl');
+    layers.appendChild(link);
+  }
+
 })
