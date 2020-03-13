919e4afef0df3d41eb191be07e1fe94fac052cc2
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2020-03-13 15:20:20 -0400

    styled subway lines in standard mta colors. used ['match, ['get', 'name']],

--- js/scripts.js
+++ js/scripts.js
@@ -14,40 +14,50 @@ var map = new mapboxgl.Map({
 // $('#legendCandidate').hide();
 
 // adding zoom and panning control
-var nav = new mapboxgl.NavigationControl();
-map.addControl(nav, 'top-left');
+var nav = new mapboxgl.NavigationControl()
+map.addControl(nav, 'top-left')
 
 // source geojson hosted on github
 var busesUrl = 'https://raw.githubusercontent.com/nikikokkinos/Data/master/QueensBusRoutes.geojson'
-
 var bikelaneUrl = 'https://raw.githubusercontent.com/nikikokkinos/Data/master/QueensProtectedBikeLanes.geojson'
-
 var citibikeUrl = 'https://raw.githubusercontent.com/nikikokkinos/Data/master/QnsCitiBike_Stations.geojson'
+var subwaylineUrl = 'https://raw.githubusercontent.com/nikikokkinos/Data/master/QueensSubwayLines.geojson'
+var subwaystopUrl = 'https://raw.githubusercontent.com/nikikokkinos/Data/master/QueensSubwayStops.geojson'
 
 // functions to be performed on load
 map.on('load', function() {
 
-  map.addSource('Bus', {
+  map.addSource('bus', {
     'type': 'geojson',
     'data': busesUrl
-    });
+    })
 
-  map.addSource('Bike', {
+  map.addSource('bike', {
     'type': 'geojson',
     'data': bikelaneUrl
-    });
+    })
 
   map.addSource('citiBike', {
     'type': 'geojson',
     'data': citibikeUrl
-    });
+    })
+
+  map.addSource('subwaylines', {
+    'type': 'geojson',
+    'data':subwaylineUrl
+  })
+
+  map.addSource('subwaystops', {
+    'type': 'geojson',
+    'data': subwaystopUrl
+  })
 
   map.getCanvas().style.cursor = 'default'
 
   map.addLayer({
     'id': 'Buses',
     'type': 'line',
-    'source': 'Bus',
+    'source': 'bus',
     'layout': {
       'line-join': 'round',
       'line-cap': 'round',
@@ -70,7 +80,7 @@ map.on('load', function() {
   map.addLayer({
     'id': 'Bikes',
     'type': 'line',
-    'source': 'Bike',
+    'source': 'bike',
     'layout': {
       'line-join': 'round',
       'line-cap': 'round',
@@ -95,21 +105,72 @@ map.on('load', function() {
     }
 })
 
+  map.addLayer({
+    'id': 'SubwayLines',
+    'type': 'line',
+    'source': 'subwaylines',
+    'layout': {
+      'line-join': 'round',
+      'line-cap': 'round',
+      'visibility': 'none',
+      },
+    'paint': {
+      'line-width': 2.5,
+      'line-color': ['match', ['get', 'name'],
+          'M', '#ff9500',
+          'F', '#ff9500',
+          'F-M', '#ff9500',
+          'A', '#0008ff',
+          'E', '#0008ff',
+          'J', '#633701',
+          'J-Z', '#633701',
+          'N', '#ffe940',
+          'W', '#ffe940',
+          'N-W', '#ffe940',
+          'N-W-R', '#ffe940',
+          'R', '#ffe940',
+          'L', '#575757',
+          'G', '#7ec261',
+          '7', '#cd75d9',
+          'S', '#575757',
+          '#ccc'
+        ]
+      }
+    })
+// "fill-color": ["step",["get","density"],"#ffeda0",10,"#ffeda0",20,"#fed976",50,"#feb24c",100,"#fd8d3c",200,"#fc4e2a",500,"#e31a1c",1000,"#bd0026"]
+
+  map.addLayer({
+    'id': 'SubwayStops',
+    'type': 'circle',
+    'source': 'subwaystops',
+    'layout': {
+      'visibility': 'none',
+    },
+    'paint': {
+      'circle-radius': 4,
+      'circle-color': '#000000'
+    }
+  })
+
   var radioButton = $('#layerToggle')
 
   radioButton.on("click", function () {
     if (document.getElementById('buses').checked) {
-        map.setLayoutProperty('Buses', 'visibility', 'visible');
-    } else { map.setLayoutProperty('Buses', 'visibility', 'none');
+        map.setLayoutProperty('Buses', 'visibility', 'visible')
+    } else { map.setLayoutProperty('Buses', 'visibility', 'none')
     } if (document.getElementById('bikelanes').checked) {
-        map.setLayoutProperty('Bikes', 'visibility', 'visible');
-    } else { map.setLayoutProperty('Bikes', 'visibility', 'none');
+        map.setLayoutProperty('Bikes', 'visibility', 'visible')
+    } else { map.setLayoutProperty('Bikes', 'visibility', 'none')
     } if (document.getElementById('citibike').checked) {
-        map.setLayoutProperty('CitiBike', 'visibility', 'visible');
-    } else { map.setLayoutProperty('CitiBike', 'visibility', 'none');
+        map.setLayoutProperty('CitiBike', 'visibility', 'visible')
+    } else { map.setLayoutProperty('CitiBike', 'visibility', 'none')}
+    if (document.getElementById('subwaylines').checked) {
+        map.setLayoutProperty('SubwayLines', 'visibility', 'visible')
+    } else { map.setLayoutProperty('SubwayLines', 'visibility', 'none')
+    } if (document.getElementById('subwaystops').checked) {
+        map.setLayoutProperty('SubwayStops', 'visibility', 'visible')
+    } else { map.setLayoutProperty('SubwayStops', 'visibility', 'none')
     }
-  });
-
-  
+  })
 
 })
